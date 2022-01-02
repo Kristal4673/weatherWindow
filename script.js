83bd1ca63f4d89ff0c2de41553661e7fe1976b48
Crystal Soto-Estrada <Csoto.lacombe@gmail.com>
2022-01-02 13:39:08 -0600

    start of variables for script and outline of html

---
+++ script.js
@@ -0,0 +1,13 @@
+
+//Declare a variable to store the searched city
+var city = "";
+// variable declaration
+var citySearch = $("#citySearch");
+var clearButton = $("#btnSearch");
+var clearButton = $("#btnClear");
+var cityCurrent = $("#cityCurrent");
+var tempCurrent = $("#tempCurrent");
+var humidityCurrent = $("#humidityCurrent");
+var windSpeedCurrent = $("#windSpeedCurrent");
+var indexUV = $("#indexUV");
+var cityPool = []; 
